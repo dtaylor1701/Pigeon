d1811055690b99c66f88487a8bfce1b40be720c2
David Taylor <dktaylor1701@gmail.com>
2024-07-23 22:36:26 -0400

    Updated response error

--- Sources/Pigeon/ServiceError.swift
+++ Sources/Pigeon/ServiceError.swift
@@ -2,5 +2,9 @@ import Foundation
 
 public enum ServiceError: Error {
   case invalidURL
-  case responseError(ResponseData)
+  case responseError(response: HTTPURLResponse, body: String?)
+  static func responseError(_ responseData: ResponseData) -> ServiceError {
+    return .responseError(
+      response: responseData.response, body: String(data: Data(responseData.data), encoding: .utf8))
+  }
 }
